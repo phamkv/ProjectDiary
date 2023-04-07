79db0258bb029321b84c2e22612231eb644f4743
Kevin Hai Nam Pham <pham@tu-berlin.de>
2023-04-07 18:19:08 +0200

    Feat: Change structure for routing

---
+++ frontend/src/lib/NavigateButton.svelte
@@ -0,0 +1,13 @@
+<!-- Button.svelte -->
+<script>
+  import { navigate } from 'svelte-routing';
+
+  export let label;
+  export let goto;
+
+  function handleClick() {
+    navigate(`${goto}`);
+  }
+</script>
+
+<button on:click={handleClick}>{label}</button>
---
+++ frontend/src/routes/Signin.svelte
@@ -0,0 +1,7 @@
+<script>
+  // import any necessary modules here
+</script>
+
+<h1>Login</h1>
+
+<!-- Add your login form here -->
